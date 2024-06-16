9f1c438a3a72033ed2f85b00aca2125fdaccdccd
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-16 13:45:35 -0600

    feat: add icon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -27,13 +28,17 @@ const Hero = () => {
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl "
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Reza, a Next.js Developer based in Canada.
           </p>
           <a href="#about">
-            <MagicButton title="Show my work" />
+            <MagicButton 
+            title="Show my work" 
+            icon={<FaLocationArrow />}
+            position="right"
+            />
           </a>
         </div>
       </div>
