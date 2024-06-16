c173dff4d3673521be0f654f52df537a013e9b0a
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-16 16:56:35 -0600

    fix: resolved grid sizing issue

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,19 +6,30 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
-          <BentoGridItem
-            id={id}
-            key={id}
-            title={title}
-            description={description}
-            className={className}
-            img={img}
-            imgClassName={imgClassName}
-            titleClassName={titleClassName}
-            spareImg={spareImg}
-          />
-        ))}
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,7 +10,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-5 gap-8 max-w-7xl mx-auto responsive",
         className
       )}
     >
@@ -43,7 +43,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1] overflow-hidden",
         className
       )}
       style={{
