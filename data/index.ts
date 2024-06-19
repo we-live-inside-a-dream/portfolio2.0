6374d4cf000b4291c11b3f5d1c8e1299edba1ed6
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-19 06:43:32 -0600

    fix

--- data/index.ts
+++ data/index.ts
@@ -120,31 +120,31 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Reza was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Reza's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Reza is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Reza was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Reza's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Reza is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Reza was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Reza's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Reza is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Reza was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Reza's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Reza is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Reza was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Reza's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Reza is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
