17423e692a40f0926b8e405d67a925d81e136543
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-19 03:43:26 -0600

    fix

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,10 +1,9 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// sentry.server.config.ts
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
+// Create a configuration object and use type assertion
+const sentryConfig: any = {
   dsn: "https://010a8c9a74f3a2ccbd101629ac856861@o4507451471691776.ingest.us.sentry.io/4507451475689472",
 
   // Adjust this value in production, or use tracesSampler for greater control
@@ -13,7 +12,11 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
+  // Custom property to disable telemetry
+  telemetry: false,
+
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-  
-});
+};
+
+Sentry.init(sentryConfig);
