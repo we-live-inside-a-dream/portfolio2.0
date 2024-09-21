d11c75d7647e7105614e18e356e4dc990ddc4758
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 10:04:37 -0600

    fix: resolving issue with other buttons

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -8,19 +8,20 @@ const MagicButton = ({
   otherClasses,
 }: {
   title: string;
-  icon?: React.ReactNode;
-  position?: string;
+  icon: React.ReactNode;
+  position: string;
   handleClick?: () => void;
   otherClasses?: string;
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className=" absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 z-0 ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 z-0 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
@@ -29,5 +30,6 @@ const MagicButton = ({
     </button>
   );
 };
+
 
 export default MagicButton;
